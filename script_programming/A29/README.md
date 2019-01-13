d491696c8c5088f1f3119a395b63ced0a1eff07a
Pätsi Teemu <K9100@student.jamk.fi>
2019-01-14 01:29:53 +0200

    Initial commit

---
+++ script_programming/A29/README.md
@@ -0,0 +1,4 @@
+### Assignment 29
+
+<p>This program prints all files contents like *nix command 'cat'</p>
+<p>Usage from command prompt: 'python asgmt29 <filename>'</p>
---
+++ script_programming/A29/asgmt29.py
@@ -0,0 +1,20 @@
+import sys
+
+#Is there right amount of parameters
+if len(sys.argv) != 2:
+    print ("Give only one filename (e.g. 'ass29.py text1.txt')")
+
+else:
+    #Test if file exists
+    try:
+        temp = open(sys.argv[1], "r")
+    except:
+        print ("File not found")
+    else:
+        #Open file and read its lines
+        f = open(sys.argv[1], "r")
+        temp_text = f.readlines()
+
+        #Remove .readlines() -method made characters between lines
+        text = ("".join(temp_text))
+        print (text)
