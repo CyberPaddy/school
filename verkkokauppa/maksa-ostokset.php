160d94fc6571e1de78d8990bee3d8e0df4af346b
Teemu <44954973+CyberPaddy@users.noreply.github.com>
2019-03-29 01:18:18 +0200

    Added elseif statement for free products in cart

--- verkkokauppa/maksa-ostokset.php
+++ verkkokauppa/maksa-ostokset.php
@@ -2,10 +2,14 @@
 
 require_once('assets/navbar.php');
 
+# Jos ostokset maksoivat jotain
 if ($maksettu) {
-  if ($less != 0) echo "$lessEcho<p><b>Ostokset maksettu!</p></b>";
+  if ($less != 0) echo "$lessEcho<p><b>Ostokset maksettu!</b></p>";
   echo "<p>Jos ostit pelejä, löydät ne <a href='pelit.php'>pelikirjastostasi</a></p>";
 }
+
+# Jos ostokset olivat ilmaisia
+elseif ($ilmainen) echo "<p>Tuotteet lisätty <a href='pelit.php'>pelikirjastoosi</a></p>";
 
 # Jos tyhjennyspainiketta on painettu, tyhjennä ostoskori
 elseif (isset($_POST['tyhjennaBtn'])) {
@@ -15,6 +19,7 @@ elseif (isset($_POST['tyhjennaBtn'])) {
 
 }
 
+# Jos sivulle tultiin muuta kautta kuin ostoskorista
 elseif (!isset($_POST['maksaBtn'])) echo "<p><b><a href='tuotteet.php'>Lisää tuotteita</a> tai mene <a href='ostoskori.php'>ostoskoriin</a></p></b>";
 
 else echo "<p>Maksu epäonnistui</p>";
