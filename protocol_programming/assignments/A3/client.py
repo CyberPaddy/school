5d6491d487852004894e5fb25551187475f83b05
Teemu Pätsi <teemu@paddy.fi>
2019-09-30 22:50:17 +0300

    Completed assignment 3

--- protocol_programming/assignments/A3/client.py
+++ protocol_programming/assignments/A3/client.py
@@ -1,20 +1,43 @@
-# Start copy from assignment page
+from my_utils import get_header_and_message_as_bytes, recv_from_socket, connection_refused_error, general_exception, send_message_to_socket
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 8888))
-# End of copy
+def main(HOST, PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-message = "hello world"
+    try:
+        s.connect((HOST, PORT))
+    
+    except ConnectionRefusedError:
+        connection_refused_error() # Prints error message and exit program (connection to server failed)
+    
+    except Exception as e:
+        general_exception(e) # Handle all other exceptions
+    
+    message = "hello world"
+    
+    # get_header_and_message_as_bytes returns message to be sent in bytes
+    # including 2 byte header which implies message length
 
-from my_utils import msg_to_socket, recv_from_socket
+    print ("Sending message to server:", message)
+    send_message_to_socket(s, message) # Parses message to correct form and makes sure everything will be sent
+    
+    # recv_from_socket handles receiving all data from client
+    # and returns the received message 
+    print("Received data from server:", recv_from_socket(s))
+        
+    s.close() # Close the connection
 
-# msg_to_socket returns message to be sent in bytes
-# including 2 byte header which implies message length
-s.send(msg_to_socket(message))
 
-# recv_from_socket handles receiving all data from client
-# and returns the received message 
-print(recv_from_socket(s))
+import sys
+# Program needs to have two command line arguments (host and port)
+if __name__ == "__main__" and len(sys.argv) == 3:
+    try:
+        PORT = int(sys.argv[2])
+    except ValueError:
+        print ("Please give integer value as <PORT>\nSyntax: client.py <HOST> <PORT>")
+        exit(1)
 
-s.close() # Close the connection
+    main(sys.argv[1], PORT) # Args: HOST, PORT
+
+elif __name__ == "__main__":
+    print("Please give two command line arguments!\nSyntax: client.py <HOST> <PORT>")
--- protocol_programming/assignments/A3/server.py
+++ protocol_programming/assignments/A3/server.py
@@ -1,24 +1,49 @@
 import socket
 
-def main():
+def main(HOST, PORT):
     # Start of copy from assignment page
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("localhost", 8888))
-    sock.listen(5)
-    (client, addr) = sock.accept()
-    print("Received a connectioni from ", addr)
-    # End of copy
-   
-    from my_utils import recv_from_socket, msg_to_socket
-    # recv_from_socket handles receiving all data from client
-    # and returns the received message
-    received_message = recv_from_socket(client)
-
-    print(received_message)
     
-    # msg_to_socket returns message to be sent in bytes
-    # including 2 byte header which implies message length
-    client.send(msg_to_socket(received_message))
+    try:
+        sock.bind((HOST, PORT))
+    except OSError:
+        print ("Port", PORT, "is already occupied by another process. Please use different port!")
+        exit(1)
+    
 
-if __name__ == "__main__":
-    main()
+    sock.listen(5)
+
+    while True:
+        (client, addr) = sock.accept()
+        print("Received a connection from ", addr)
+        # End of copy
+       
+        # recv_from_socket handles receiving all data from client
+        # and returns the received message
+        from my_utils import recv_from_socket, get_header_and_message_as_bytes
+        received_message = recv_from_socket(client)
+
+        print("Received data from client:", received_message)
+        
+        # Echo message back to client
+        # get_header_and_message_as_bytes returns message to be sent in bytes
+        # including 2 byte header which implies message length
+        print ("Sending message back to the client")
+        client.send(get_header_and_message_as_bytes(received_message))
+
+    print ("\nClosing the server...")
+    sock.close()
+
+import sys
+# Program needs to have two command line arguments (host and port)
+if __name__ == "__main__" and len(sys.argv) == 3:
+    try:
+        PORT = int(sys.argv[2])
+    except ValueError:
+        print ("Please give integer value as <PORT>\nSyntax: server.py <HOST> <PORT>")
+        exit(1)
+
+    main(sys.argv[1], PORT) # Args: HOST, PORT
+
+elif __name__ == "__main__":
+    print("Please give two command line arguments!\nSyntax: server.py <HOST> <PORT>")
