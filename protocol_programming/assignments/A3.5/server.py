f5e8adbcb4ae58950165c57aa25465b24f04c32e
Teemu Pätsi <teemu@paddy.fi>
2019-10-19 03:46:36 +0300

    Added third command line argument representing the receive folder

--- protocol_programming/assignments/A3.5/server.py
+++ protocol_programming/assignments/A3.5/server.py
@@ -1,15 +1,19 @@
 import socket
 
 # Creates new file to 'received-files' -folder
-def create_new_file(name, contents):
-    folder = 'received-files/'
+def create_new_file(name, contents, folder):
+    from my_utils import format_folder  # Returns folder path with only forward slashes
+    folder = format_folder(folder)
+
+    from pathlib import Path    # Path converts forward slashes into the correct kind of slash for the OS
     try:
-        new_file = open( (folder + name), 'wb')
+        new_file = open( Path(folder) / name, 'wb') 
     except FileNotFoundError:
         print("Folder '" + folder + "' doesn't exist\nServer is shutting down...")
         exit(1)
-    except PermissionError:
+    except PermissionError as e:
         print("Current user does not have write permission to folder '" + folder + "'\nServer is shutting down...")
+        print (e)
         exit(1)
     except IOError:
         print("File", name, "could not be created to '" + folder + "' -folder\nServer is shutting down...")
@@ -19,7 +23,7 @@ def create_new_file(name, contents):
     new_file.close()
     print ("Successfully created new file", name, "to received_files -folder!")
 
-def main(HOST, PORT):
+def main(RECV_FOLDER, HOST, PORT):
     s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     try:
@@ -32,24 +36,27 @@ def main(HOST, PORT):
 
     while True:
         (client, addr) = s.accept()
-        print("Received a connection from ", addr)
+        print("\nReceived a connection from ", addr)
        
         # recv_from_socket handles receiving all data from client
         # and returns the name of the file and its contents
         from my_utils import recv_from_socket
         file_name, received_file = recv_from_socket(client)
 
-        print ("\nName of the received file:", file_name)
-        create_new_file(file_name, received_file) # Creates new file to 'received-files' -folder
+        if len(file_name) != 0:
+            print ("\nName of the received file:", file_name)
+            create_new_file(file_name, received_file, RECV_FOLDER)
+        else:
+            print("Client did not send any file")
         
     print ("\nClosing the server...")
     s.close()
 
 import sys
 # Program needs to have two command line arguments (host and port)
-if __name__ == "__main__" and len(sys.argv) == 3:
+if __name__ == "__main__" and len(sys.argv) == 4:
     try:
-        PORT = int(sys.argv[2])
+        PORT = int(sys.argv[3])
         # Do not use well-known ports 0-1023
         if PORT < 1024 or PORT > 65535:
             raise ValueError
@@ -57,7 +64,7 @@ if __name__ == "__main__" and len(sys.argv) == 3:
         print ("Please give integer value between 1024-65535 as <PORT>\nSyntax: server.py <HOST> <PORT>")
         exit(1)
 
-    main(sys.argv[1], PORT) # Args: HOST, PORT
+    main(sys.argv[1], sys.argv[2], PORT) # Args: RECV_DIR HOST, PORT
 
 elif __name__ == "__main__":
     print("Please give two command line arguments!\nSyntax: server.py <HOST> <PORT>")
