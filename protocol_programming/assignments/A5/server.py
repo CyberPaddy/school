9abacff4293edd75f5c83f2f32c185538fa36cdb
CyberPaddy <teemu@paddy.fi>
2019-11-11 14:54:07 +0200

    Updated syntax. Still a lot to do.

--- protocol_programming/assignments/A5/server.py
+++ protocol_programming/assignments/A5/server.py
@@ -1,14 +1,16 @@
 import os, sys, socket, my_ftp
 from pathlib import Path    # Converts forward slashes into the correct kind of slash for OS
 
+SEQ_NUMS = []
+
 def test_one_parameter_functions(command, request, mark):
     if request[mark] != ' ':
-        print("SP Missing")
+        print("SP Missing 1")
         return []
 
     COMMAND_PARAM = ''
 
-    for i in range(mark+1, len(request)-3):
+    for i in range(mark+1, len(request)-5):
         if request[i] == ' ' or request[i] == ';': # These commands should not have multiple parameters
             return []
         COMMAND_PARAM += request[i]
@@ -18,14 +20,17 @@ def test_one_parameter_functions(command, request, mark):
 
 # Syntax example: LS text.txt pic.jpg;CRLF
 def test_ls_syntax(command, request, mark):
+
     if request[mark] != ' ':
-        print("SP Missing")
+        print (request[mark])
+        print (type (request[mark]))
+        print("SP Missing 2")
         return []
     
     files = []
     COMMAND_PARAM = ''
 
-    for i in range(mark+1, len(request)-3):
+    for i in range(mark+1, len(request)-4):
         if request[i] == ';': # LS command should only contain SP, not SEMICOLON
             return []
         if request[i] == ' ':
@@ -51,17 +56,21 @@ def test_file_syntax(request, mark):
 
 
 def test_command_parameters(command, request, mark):
-    if len(request) < mark+3: # LIST;CRLF
+
+    print (command, request, mark)
+
+    if len(request) < mark+4: # LIST;CRLF<SEQ_BYTES>
         return []   # Empty list means syntax error 501
     
-    if (request[-3:] != ';\r\n'):
+    if (request[-5:-2] != ';\r\n'):
         print(";CRLF missing:")
         return []
 
     # LIST and QUIT commands do not want parameters
     if command == 'LIST' or command == 'QUIT':
-        if len(request) > mark+3:
+        if len(request) > mark+4:
             return []
+        print ("JEE")
         return [request]
 
     COMMAND_PARAM = ''
@@ -78,10 +87,12 @@ def test_command_parameters(command, request, mark):
     
 
 
-# SYNTAX: <COMMAND>[SP<COMMAND_PARAM>[;<DATA_SIZE>;<DATA>]];CRLF
+# SYNTAX: <COMMAND>[SP<COMMAND_PARAM>[;<DATA_SIZE>;<DATA>]];SEQ<SEQ_BYTES>CRLF
 def parse_request(request):
     
+    print (request)
     real_command = False
+    seq_bytes = bytes(request[:-2], 'utf-8')
     
     # Test if the command is valid
     for valid_command in my_ftp.COMMANDS:
@@ -93,15 +104,13 @@ def parse_request(request):
     
     # function_params is empty if <COMMAND> was not recognized
     if not real_command:
-        return [], b'ERROR 500;\r\n'  # Command not recognised
+        return [], ( b'ERROR 500;\r\n' + seq_bytes ) # Command not recognised
     
-    print (request[mark:])
-
     function_params = test_command_parameters(COMMAND, request, mark)
     if function_params == []:
-        return [], b'ERROR 501;\r\n'
+        return [], ( b'ERROR 501;\r\n' + seq_bytes )
     
-    return function_params, b'ACK 200;\r\n'
+    return function_params, ( b'ACK 200;\r\n' + seq_bytes )
     
 
 def main(HOST, PORT, PATH):
@@ -118,16 +127,16 @@ def main(HOST, PORT, PATH):
 
             request = ''
             while True:
-                request += client.recv(1024).decode('utf-8')
-                if request[-2:] == '\r\n':
-                    print ("Client's command:", request)
+                request += client.recv(1024).decode()
+                if request[-4:-2] == '\r\n':
+                    print ("Client's command:", request[:-4]) # Don't print CRLF and <SEQ_BYTES>
                     break
 
             function_params, ack_bytes = parse_request(request)
             client.sendall(ack_bytes)
 
-            client.sendall(ack_bytes)
-            
+
+
 
 
 if __name__ == '__main__' and len(sys.argv) == 4:
@@ -145,4 +154,4 @@ if __name__ == '__main__' and len(sys.argv) == 4:
     main(HOST, PORT, PATH)
 
 elif __name__ == '__main__':
-    main('localhost', 13337, 'share/')
+    main('localhost', 13338, 'share/')
