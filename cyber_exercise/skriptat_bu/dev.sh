58b23475bfc07a572aba301e043de93c324a9868
CyberPaddy <teemu@paddy.fi>
2019-11-14 13:57:34 +0200

    Removed cyber exercise website from git

--- cyber_exercise/skriptat_bu/dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -fr ../../dev-tietokannat/ && cp -a ../ ../../dev-tietokannat/
--- cyber_exercise/sql_login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-db="K9100_1"
-
-# Kirjaudu tietokantaan 
-mysql -u K9100 -pM7fMqMcrHpRuRMKlJkYYizh0a5d0dw41 -D $db
