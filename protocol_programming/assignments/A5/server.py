168fcd2c5d1017d7a86ab005474b4e5b9c3128eb
Teemu Pätsi <teemu@paddy.fi>
2019-12-06 03:13:33 +0200

    Server now sends the right ACK or ERROR to client immiediately after receiving REQUEST

--- protocol_programming/assignments/A5/server.py
+++ protocol_programming/assignments/A5/server.py
@@ -1,109 +1,79 @@
 import os, sys, socket, my_ftp
 from pathlib import Path    # Converts forward slashes into the correct kind of slash for OS
 
-SEQ_NUMS = []
+# Test if the received command is in VALID_LIST
+def is_valid_command(command, VALID_LIST):
+    for valid_command in VALID_LIST:
+        mark = len(valid_command)
+        parsed_command = command[:mark]
+        if parsed_command == valid_command:
+            return True, parsed_command, mark
+    return False, None, None
 
 def test_one_parameter_functions(command, request, mark):
     if request[mark] != ' ':
-        print("SP Missing for one parameter function")
+        print("SP Missing for one parameter function", command)
         return []
 
-    COMMAND_PARAM = ''
+    REQUEST_PARAM = ''
 
-    for i in range(mark+1, len(request)-1):
+    for i in range(mark+1, len(request)-3):
+        print (request[i])
         if request[i] == ' ' or request[i] == ';': # These commands should not have multiple parameters
             return []
-        COMMAND_PARAM += request[i]
+        REQUEST_PARAM += request[i]
 
-    print ("COMMAND_PARAM:", COMMAND_PARAM)
-    return [command, COMMAND_PARAM]
+    print ("REQUEST_PARAM:", REQUEST_PARAM)
+    return [command, REQUEST_PARAM]
 
-# Syntax example: LS text.txt pic.jpg;CRLF
-def test_ls_syntax(command, request, mark):
 
-    if request[mark] != ' ':
-        print (request[mark])
-        print (type (request[mark]))
-        print("SP Missing for LS")
-        return []
+def test_request_params(command, request, mark):
+
+    print ("test_request_params", request)
+
+    if len(request) < mark+3: # LIST;CRLF
+        print("Some part missing")
+        return []   # Empty list means syntax error 502
     
-    files = []
-    COMMAND_PARAM = ''
-
-    for i in range(mark+1, len(request)):
-        if request[i] == ';': # LS command should only contain SP, not SEMICOLON
-            return []
-        if request[i] == ' ':
-            files.append(COMMAND_PARAM)
-            COMMAND_PARAM = ''
-            continue
-
-        COMMAND_PARAM += request[i]
-
-    files.append(COMMAND_PARAM)
-
-    print ("FILES =", files)
-    return [command, files]
-
-def test_file_syntax(request, mark):
-    return
-
-
-def test_command_parameters(command, request, mark):
-
-    print (command, request, mark)
-
-    if len(request) < mark: # LIST;CRLF
-        return []   # Empty list means syntax error 501
-    
-    if (request[-5:-2] != ';\r\n'):
+    if (request[-3:] != ';\r\n'):
         print(";CRLF missing:")
         return []
 
     # LIST and QUIT commands do not want parameters
     if command == 'LIST' or command == 'QUIT':
-        if len(request) > mark+4:
+        if len(request) > mark+3:
             return []
         print ("Good command:", command)
         return [request]
 
-    COMMAND_PARAM = ''
-
-    # These commands allow only one parameter
-    if command == 'DOWNLOAD' or command == 'ACK' or command == 'ERROR':
+    # DOWNLOAD only allows one parameter
+    if command == 'DOWNLOAD':
         return test_one_parameter_functions(command, request, mark)
 
-    elif command == 'LS':
-        return test_ls_syntax(command, request, mark)
-
-    return test_file_syntax(command, request, mark) # The only remaining command to test is FILE
+    return [] 
 
 
 # SYNTAX: <COMMAND>[SP<COMMAND_PARAM>[;<DATA_SIZE>;<DATA>]];CRLF
 def parse_request(request):
-    
-    real_command = False
-
-    # Test if the command is valid
-    for valid_command in my_ftp.COMMANDS:
-        mark = len(valid_command)
-        COMMAND = request[:mark]
-        if COMMAND == valid_command:
-            real_command = True
-            break
-    
-    # function_params is empty if <COMMAND> was not recognized
+    # Is received request a valid COMMAND
+    real_command = is_valid_command(request, my_ftp.COMMANDS)
     if not real_command:
-        return [], ( b'ERROR 500;\r\n' ) # Command not recognised
-    
-    function_params = test_command_parameters(COMMAND, request, mark)
+        return [], ( b'ERROR 500;\r\n' ) # 500 -> COMMAND unrecognized
+
+    # Test if the command is valid REQUEST. Variable 'mark' is the end index for <COMMAND>
+    real_request, command, mark = is_valid_command(request, my_ftp.REQUESTS)
+    if not real_request:
+        return [], ( b'ERROR 501;\r\n' ) # 501 -> Not implemented
+
+    # Return received REQUEST and its params in a list. Empty list = syntax error
+    function_params = test_request_params(command, request, mark)
     if function_params == []:
-        return [], ( b'ERROR 501;\r\n' )
+        return [], ( b'ERROR 502;\r\n' ) # 502 -> Syntax error
     
-    return function_params, ( b'ACK 200;\r\n' )
+    return function_params, ( b'ACK 200;\r\n' ) # 200 -> Alles gud
 
 def handle_function(param_list):
-
+    return
 
 def main(HOST, PORT, PATH):
     print ("Starting server!\nHOST: " + HOST + "\nPORT: " + str(PORT) + "\nPATH: " + PATH)
@@ -120,15 +90,16 @@ def main(HOST, PORT, PATH):
             request = ''
             while True:
                 request += client.recv(1024).decode()
-                if request[-4:-2] == '\r\n':
-                    print ("Client's command:", request[:-4]) # Don't print CRLF
+                print (bytes(request[-2:], 'utf-8'))
+                if request[-2:] == '\r\n':
+                    print ("Client's command:", request[:-3]) # Don't print CRLF
                     break
-
+            
             function_params, ack_bytes = parse_request(request)
             print("ack_bytes:",ack_bytes)
             client.sendall(ack_bytes)
 
-            if ack_bytes[-2:] == b'ACK 200;\r\n'
+            if ack_bytes == b'ACK 200;\r\n':
                 print ("function_params:",function_params)
                 handle_function(function_params)
 
